6faff987c1474d75e5b42f03b6e088219e51a89a
Marcos <marcosraach.1@gmail.com>
2023-10-15 01:52:26 -0300

    fim do rapido curso da alura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common'
+import { UserModule } from './user/user.module'
 
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule]
 })
+
 export class AppModule {}
